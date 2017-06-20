81ab631d55b79b2e6e82764e76ef065ea0123bd0
Ben Daniell <bendaniell@Bens-MacBook-Pro-2.local>
2017-06-20 11:13:31 +1000

    Support files for new viewController

--- gitTest/secondViewController.swift
+++ gitTest/secondViewController.swift
@@ -13,6 +13,8 @@ class secondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("memes")
+        
         // Do any additional setup after loading the view.
     }
 
